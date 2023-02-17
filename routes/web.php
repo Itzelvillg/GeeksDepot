bbfb773f929582816559fca9dee18193fc32e1ac
Itzel Villaseñor <110121112+Itzelvillg@users.noreply.github.com>
2023-02-16 22:15:55 -0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,15 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 
 
 
 
+// Rutas para el usuario
 
 
 Route::view('/','index' )->name('inicio');
@@ -28,6 +21,22 @@ Route::view('crearcuenta','secciones.crearcuenta' )->name('nuevacuenta');
 Route::view('micuenta','secciones.vistausuario' )->name('usuario');
 
 
+// Rutas para el admin
+
+
+Route::view('administracion','admin.index' )->name('admin');
+Route::view('userAdm','admin.secciones.usuario.index' )->name('admuser');
+Route::view('blogAdm','admin.secciones.blog.index' )->name('admblog');
+Route::view('brandAdm','admin.secciones.brand.index' )->name('admbrand');
+Route::view('productAdm','admin.secciones.product.index' )->name('admproduct');
+Route::view('purchaseAdm','admin.secciones.purchase.index' )->name('admpurchase');
+Route::view('imgAdm','admin.secciones.image.index' )->name('admimage');
+
+
+// Rutas 
+
+Route::resource('/productAdmin', ProductController::class);
+Route::resource('/blogAdmin',BlogController::class);
 
 
 
